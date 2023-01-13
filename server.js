c934d15866324b7aa63d5e83873d85a8312d2c62
Abdulrahman Emad <65741884+abdulrahman3mad@users.noreply.github.com>
2023-01-13 16:37:38 +0200

    some edits

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const auth = require('json-server-auth')
 const app = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 5000;
+
 
 app.db = router.db
 app.use(jsonServer.rewriter({
     "/carts/:type/:id": "/600/carts/:id",
     "/carts/:id": "/600/carts/:id",
-    "/products/": "/660/products",
     "/users/:id": "/600/users/:id",
     "/users": "/600/users"
 }))
+
 app.use(middlewares)
 app.use(auth)
 app.use(router)
-app.listen(5000)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
