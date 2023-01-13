8c843dcbcf8ccb6c6306a56ac063590b86095e6e
Abdulrahman Emad <65741884+abdulrahman3mad@users.noreply.github.com>
2023-01-13 16:32:45 +0200

    adding rotues

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const jsonServer = require('json-server');
 const auth = require('json-server-auth')
 const app = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults()
 
 app.db = router.db
-const port = process.env.PORT || 3200; // <== You can change the port
-
+app.use(jsonServer.rewriter({
+    "/carts/:type/:id": "/600/carts/:id",
+    "/carts/:id": "/600/carts/:id",
+    "/products/": "/660/products",
+    "/users/:id": "/600/users/:id",
+    "/users": "/600/users"
+}))
+app.use(middlewares)
 app.use(auth)
 app.use(router)
-app.listen(3000)
+app.listen(5000)
