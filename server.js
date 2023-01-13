68e5869eecc445822fbac8f14365d4343af5f047
Abdulrahman Emad <65741884+abdulrahman3mad@users.noreply.github.com>
2023-01-13 15:15:48 +0200

    server

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const auth = require('json-server-auth')
+const app = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
+
+app.db = router.db
 const port = process.env.PORT || 3200; // <== You can change the port
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+app.use(auth)
+app.use(router)
+app.listen(3000)
